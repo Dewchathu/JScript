9f81a5dcade8939c51135bd4ba7ff128c97ff90d
Chathura Devinda Gamage <105056329+Dewchathu@users.noreply.github.com>
2023-04-07 02:19:01 +0530

    Initial commit

---
+++ 01/script.js
@@ -0,0 +1,8 @@
+
+
+function cal(){
+    var N1 = document.getElementById("num1").value;
+    var N2 = document.getElementById("num2").value;
+    var ans = document.getElementById("answer");
+    ans.innerHTML = Number(N1)+Number(N2);
+}
